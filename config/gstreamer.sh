3318010d52947f892403191d4e11edf7123dce05
Dirk P. Janssen <djanssen@pivotal.io>
2017-04-19 12:42:09 -0700

    Flipped boardcam around (fixed port number in index)

--- config/gstreamer.sh
+++ config/gstreamer.sh
@@ -13,6 +13,6 @@ gst-launch-1.0 \
     videomixer name=mix sink_0::alpha=1  sink_1::ypos=460 sink_1::xpos=620 sink_1::alpha=.5  sink_2::ypos=320 sink_2::xpos=620 !   \
            videoconvert ! x264enc tune=zerolatency  ! \
            mpegtsmux ! hlssink max-files=5 async-handling=true target-duration=5   \
-    avfvideosrc device-index=${boardcam} !  video/x-raw , width=800 !  mix.   \
+    avfvideosrc device-index=${boardcam} !  video/x-raw , width=800 ! videoflip method=2 !  mix.   \
     avfvideosrc device-index=${scenecama} !  videoscale method=0 ! video/x-raw, width=160, height=120 ! mix.  \
     avfvideosrc device-index=${scenecamb} !  videoscale method=0 ! video/x-raw, width=160, height=120 ! mix.
